cdfdff165a6cab5b935aa8681db85a5b74acf4e0
unknown <joispersonalemail@gmail.com>
2023-01-21 16:54:53 -0800

    updated js

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Get references to the #generate element
 function containsOnlyNumbers(x) {
   return /^\d+$/.test(x);
 }
 
-function generatePassword(){
+function generatePassword() {
   let Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   let Lowercase = "abcdefghijkomnopqrstuvwxyz"
   let Numeric = "0123456789"
-  let Special ="!@#$%^&*()_+<>?[]" ;
+  let Special = "!@#$%^&*()_+<>?[]";
   let password = "";
   let characters = "";
- 
+
 
   do {
     var NumberOfCharacters_str = window.prompt("select password size between 8 and 128 characters");
 
-    if(!NumberOfCharacters_str){
+    if (!NumberOfCharacters_str) {
       return;
     }
 
-    if(containsOnlyNumbers(NumberOfCharacters_str)===false){
+    if (containsOnlyNumbers(NumberOfCharacters_str) === false) {
       var EnterNumber = window.confirm("please enter numbers");
-      if(!EnterNumber){
+      if (!EnterNumber) {
         return;
       }
-    }else if (Number(NumberOfCharacters_str)<8 || Number(NumberOfCharacters_str)>128){
+    } else if (Number(NumberOfCharacters_str) < 8 || Number(NumberOfCharacters_str) > 128) {
       var NumberLength = window.confirm("password size must be between 8 and 128 characters");
-      if(!NumberLength){
+      if (!NumberLength) {
         return;
       }
-    }else{
+    } else {
       break;
     }
   } while (true);
-  
+
   var checkLowercase = window.confirm("Would you like to include lowercase characters?");
-  if(checkLowercase){
+  if (checkLowercase) {
     characters = Lowercase;
   }
 
   var checkUppercase = window.confirm("Would you like to include uppercase characters?");
-  if(checkUppercase){
+  if (checkUppercase) {
     characters = characters + Uppercase;
   }
-  
-  var checkNumeric= window.confirm("Would you like to include numeric characters?");
-  if(checkNumeric){
+
+  var checkNumeric = window.confirm("Would you like to include numeric characters?");
+  if (checkNumeric) {
     characters = characters + Numeric;
   }
 
   var checkSpecial = window.confirm("Would you like to include special characters?");
-  if(checkSpecial){
+  if (checkSpecial) {
     characters = characters + Special;
   }
 
- 
-  for (let i=0; i<Number(NumberOfCharacters_str); i++){
-    password += characters.charAt(Math.floor(Math.random() * characters.length));    
+
+  for (let i = 0; i < Number(NumberOfCharacters_str); i++) {
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
   }
 
   return password;
